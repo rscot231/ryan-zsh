606ce9f304ff955b91f40775317d56aa5f69abd4
Ryan Scott <rscot231@gmail.com>
2021-03-20 15:08:59 -0600

    JDK 16

--- machines/ryan-personal-mbp.zsh
+++ machines/ryan-personal-mbp.zsh
@@ -6,4 +6,4 @@ plugins+=(
 export GOPATH=$HOME/go
 
 # JAVA STUFF
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-16.jdk/Contents/Home
